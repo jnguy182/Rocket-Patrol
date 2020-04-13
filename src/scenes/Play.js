8e67fa2bf0ba58a68250fb2f77ede69e0bfd3dbd
Jake Nguyen <jnguy182@ucsc.edu>
2020-04-12 19:26:40 -0700

    Implement Rocket

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,7 +5,7 @@ class Play extends Phaser.Scene {
 
     preload() {
         // load images/tile sprites
-        this.load.image('rocket', './assets/rockets.png');
+        this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png')
     }
@@ -20,6 +20,8 @@ class Play extends Phaser.Scene {
         this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
         // green UI background
         this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
+        // add rocket (p1)
+        this.p1Rocket = new Rocket(this, game.config.width / 2 - 8, 431, 'rocket').setScale(0.5, 0.5).setOrigin(0, 0);
     }
 
     update() {
