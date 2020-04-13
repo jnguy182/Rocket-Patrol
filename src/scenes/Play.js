ebaf5c3e16e3ce11829b53be8d29e27442b3c86b
Jake Nguyen <jnguy182@ucsc.edu>
2020-04-13 15:01:35 -0700

    Implement Spaceship Class

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -22,6 +22,10 @@ class Play extends Phaser.Scene {
         this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width / 2 - 8, 431, 'rocket').setScale(0.5, 0.5).setOrigin(0, 0);
+        // add spaceships (x3)
+        this.ship01 = new Spaceship(this, game.config.width + 192, 132, 'spaceship', 0, 30);
+        this.ship02 = new Spaceship(this, game.config.width + 96, 196, 'spaceship', 0, 20).setOrigin(0, 0);
+        this.ship03 = new Spaceship(this, game.config.width, 260, 'spaceship', 0, 10).setOrigin(0, 0);
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
@@ -31,5 +35,9 @@ class Play extends Phaser.Scene {
     update() {
         this.starfield.tilePositionX -= 4;
         this.p1Rocket.update();
+        // update spaceships (x3)
+        this.ship01.update();
+        this.ship02.update();
+        this.ship03.update();
     }
 }
