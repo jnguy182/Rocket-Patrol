6ccc1d44a4adbf50797eb5babbbca5313c0d7173
Jake Nguyen <jnguy182@ucsc.edu>
2020-04-13 22:26:32 -0700

    Keeping Score

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -47,6 +47,22 @@ class Play extends Phaser.Scene {
             }),
             frameRate: 30
         });
+        // score
+        this.p1Score = 0;
+        // score display
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(69, 54, this.p1Score, scoreConfig);
     }
 
     update() {
@@ -104,5 +120,8 @@ class Play extends Phaser.Scene {
             // remove explosion sprite
             boom.destroy();
         });
+        // score increment and repaint
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score;
     }
 }
